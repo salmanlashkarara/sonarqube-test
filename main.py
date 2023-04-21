fb60b163c919c50c24f0427af131478eb04ade75
lashksal <salman.lashkarara@diconium.com>
2023-04-21 09:32:55 +0200

    add unit test

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello")
+from unittest import TestCase
+
+
+def func(a):
+    if a % 2 == 0:
+        return 2 * a
+    else:
+        return a / 2
+
+
+print("Func: ", func(3))
+
+TestCase().assertEqual(func(3), 31.5, "Expected value and actual value are not equal")
