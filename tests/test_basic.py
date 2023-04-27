93e9d2baee5b62dfbcb68de7bebe407c374c77ed
lashksal <salman.lashkarara@diconium.com>
2023-04-27 16:16:09 +0200

    add test for sub

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -8,5 +8,10 @@ def test_even_number():
 def test_odd_number():
     assert func(3) == 1.5
 
+
 def test_sum():
-    assert sum(3,2) == 5
+    assert sum(3, 2) == 5
+
+
+def test_sub():
+    assert sub(3, 2) == 1
