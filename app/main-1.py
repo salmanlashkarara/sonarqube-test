991347d640c87f6df7c3b4692f340cae603bb889
lashksal <salman.lashkarara@diconium.com>
2023-04-28 15:12:57 +0200

    enable methods

---
+++ app/main-1.py
@@ -0,0 +1,40 @@
+def func(a):
+    if a % 2 == 0:
+        return 2 * a
+    else:
+        return a / 2
+
+
+def sum(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+def div(a, b):
+    return a / b
+
+
+def multitude(a, b):
+    return a * b
+
+
+def power(a, b):
+    return a ** b
+
+
+def sum_list(lst):
+    print("Hellllo")
+    total = 0
+    for num in lst:
+        total += num
+    return total
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
